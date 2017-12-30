6709ab9c4fc6ef615f814b558b3a40418471793b
davenamin <amind@my.uri.edu>
2017-12-30 11:07:50 -0500

    *** empty log message ***

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // // CORS for getting NOAA data
 // // https://enable-cors.org/server_expressjs.html
